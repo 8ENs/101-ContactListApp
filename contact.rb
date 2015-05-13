2b5d9fb48d2883edd9b53c54a3876791bac7a5ce
8ENs <ben@coderush.ca>
2015-05-13 05:50:28 +0000

    All improvements working though need to refactor - especially printing which is WET

--- contact.rb
+++ contact.rb
@@ -1,5 +1,5 @@
 class Contact
-  attr_accessor :firstname, :lastname, :email, :id # all needed as accessor?
+  attr_accessor :firstname, :lastname, :email, :id, :phone # all needed as accessor?
  
   @@connection = nil # should there still be a class variable?
   
@@ -14,11 +14,12 @@ class Contact
 
   CONN = Contact.connection
 
-  def initialize(firstname, lastname, email, id = nil)
+  def initialize(firstname, lastname, email, id = nil, phone = [])
     @firstname = firstname
     @lastname = lastname
     @email = email
     @id = id
+    @phone = phone
   end
  
   # def to_s(contact_array) # could remove argument but would need to adjust ContactDatabase.read_contacts to return Contact objects
@@ -33,69 +34,104 @@ class Contact
   def save
     if is_new?
       result = CONN.exec_params('INSERT INTO contacts (firstname, lastname, email) VALUES ($1, $2, $3) returning id;', [@firstname, @lastname, @email])
-      @id = result[0]['id']
+      @id = result[0]['id'] # will return @id as String
     else
       CONN.exec_params('UPDATE contacts SET firstname = $1, lastname = $2, email = $3 WHERE id = $4;', [@firstname, @lastname, @email, @id])
     end
   end
 
+  def add_phone(digit_array_of_hash)
+    # COME BACK AND CHECK IF UPDATING (now only add)
+    digit_array_of_hash.each do |phone_label|
+      CONN.exec_params('INSERT INTO phones (phone, label, contact_id) VALUES ($1, $2, $3);', [phone_label[:phone], phone_label[:label], @id])
+    end
+  end
+
+  def get_phones
+    sql_string = "SELECT * FROM phones WHERE contact_id = #{@id};"
+    numbers = CONN.exec(sql_string)
+    if numbers.ntuples > 0
+      numbers.each { |phone_label| @phone << { phone: phone_label['phone'], label: phone_label['label'] } }
+    end
+  end
+
   def destroy
     CONN.exec_params('DELETE FROM contacts WHERE id = $1', [@id])
+    CONN.exec_params('DELETE FROM phones WHERE id = $1', [@id])
+  end
+
+  def self.destroy(id)
+    CONN.exec_params('DELETE FROM contacts WHERE id = $1', [id])
+    CONN.exec_params('DELETE FROM phones WHERE contact_id = $1', [id])
   end
 
   def self.find(id)
     con_obj = CONN.exec_params('SELECT * FROM contacts WHERE id = $1 LIMIT 1;', [id])
+    result = nil
     if con_obj.ntuples > 0
       result = Contact.new(con_obj[0]['firstname'], con_obj[0]['lastname'], con_obj[0]['email'], id)
+      result.get_phones
     end
-
-    # return single Contact or NIL
+    result
+    # return single Contact (or nil)
   end
  
   def self.all
     results = []
-    CONN.exec_params('SELECT * FROM contacts;').each do |tuple|
-      results << Contact.new(
+    CONN.exec('SELECT * FROM contacts;').each do |tuple|
+      # 'SELECT * FROM contacts LEFT JOIN phones ON id = contact_id;'
+      # phone_hash = { phone: tuple['phone'], label: tuple['label']}
+      temp_contact = Contact.new(
         tuple['firstname'],
         tuple['lastname'],
         tuple['email'],
         tuple['id']
+        # phone_hash
       )
+      temp_contact.get_phones
+      results << temp_contact
     end
-    results # return array of Contacts (or nil)
+    results # return array of Contacts (or empty array)
   end
  
   def self.find_all_by_lastname(lastname)
     results = []
     CONN.exec_params('SELECT * FROM contacts WHERE lastname = $1', [lastname]).each do |tuple|
-      results << Contact.new(
+      temp_contact = Contact.new(
         tuple['firstname'],
         lastname,
         tuple['email'],
         tuple['id']
       )
+      temp_contact.get_phones
+      results << temp_contact
     end
-    results # return array of Contacts (or nil)
+    results # return array of Contacts (or empty array)
   end
 
   def self.find_all_by_firstname(firstname)
     results = []
     CONN.exec_params('SELECT * FROM contacts WHERE firstname = $1', [firstname]).each do |tuple|
-      results << Contact.new(
+      temp_contact = Contact.new(
         firstname,
         tuple['lastname'],
         tuple['email'],
         tuple['id']
       )
+      temp_contact.get_phones
+      results << temp_contact
     end
-    results # return array of Contacts (or nil)
+    results # return array of Contacts (or empty array)
   end
 
   def self.find_by_email(email)
     con_obj = CONN.exec_params('SELECT * FROM contacts WHERE email = $1 LIMIT 1;', [email])
+    result = nil
     if con_obj.ntuples > 0
       result = Contact.new(con_obj[0]['firstname'], con_obj[0]['lastname'], email, con_obj[0]['id'])
+      result.get_phones
     end
+    result
     # return either single contact (or nil)
   end
 end
--- repl.rb
+++ repl.rb
@@ -5,9 +5,11 @@ if ARGV[0] == "help"
   puts "  new  - Create a new contact"
   puts "  list - List all contacts"
   puts "  show - Show a contact (eg. 'ruby main.rb show 3')"
-  puts "  findfirst - Find a contact by FIRST name (eg. 'ruby main.rb findfirst Ben')"
-  puts "  findlast - Find a contact by LAST name (eg. 'ruby main.rb findlast Sanders')" # case sensitive
-  # puts "  add_phone - Add phone (eg. 'ruby main.rb add_phone Mobile 8673368077')" # TODO BONUS
+  puts "  find_first - Find a contact by FIRST name (eg. 'ruby main.rb findfirst Ben')"
+  puts "  find_last - Find a contact by LAST name (eg. 'ruby main.rb findlast Sanders')" # case sensitive
+  puts "  find_email - Find a contact by EMAIL (eg. 'ruby main.rb find_email ben@bensanders.ca')" 
+  puts "  delete - Delete a contact (eg. 'ruby main.rb delete 3')" 
+  puts "  add_phone - Add phone (eg. 'ruby main.rb add_phone 3 Mobile 8673368077')" # TODO BONUS
 elsif ARGV[0] == "new" 
   puts "Enter email:"
   email = STDIN.gets.chomp
@@ -20,19 +22,56 @@ elsif ARGV[0] == "new"
     puts "Enter last name:"
     lastname = STDIN.gets.chomp
 
-    Contact.new(firstname, lastname, email).save
+    digit_array = []
+
+    loop do
+      puts "Enter phone (##########) - enter 'q' if done" # Error checking later
+      phone = STDIN.gets.chomp
+      break if phone.to_i <= 0
+      puts "Enter lable (eg. 'Mobile')"
+      label = STDIN.gets.chomp
+      digit_array << { phone: phone, label: label }
+    end
+
+    new_contact = Contact.new(firstname, lastname, email)
+    new_contact.save
+    new_contact.add_phone(digit_array) if digit_array.length > 0
   end
 elsif ARGV[0] == "list" 
-  # binding.pry
   array_of_contacts = Contact.all
-  array_of_contacts.each { |contact| puts "#{contact.lastname}, #{contact.firstname} (#{contact.email})" }
+  array_of_contacts.each do |contact| 
+    phone_mash = ""
+    contact.phone.each { |phone_hash| phone_mash << " | #{phone_hash[:phone]} (#{phone_hash[:label]})" }
+    puts "#{contact.id}: #{contact.lastname}, #{contact.firstname} (#{contact.email})#{phone_mash}"
+  end
 elsif ARGV[0] == "show" && ARGV[1]
-  contact = Contact.find(ARGV[1].to_i)
-  puts contact ? "#{contact.lastname}, #{contact.firstname} (#{contact.email})" : "#{ARGV[1]} (not found)"
-elsif ARGV[0] == "findfirst" && ARGV[1]
+  contact = Contact.find(ARGV[1].to_i) # do I need the '.to_i' ?
+  phone_mash = ""
+  contact.phone.each { |phone_hash| phone_mash << " | #{phone_hash[:phone]} (#{phone_hash[:label]})" } if contact
+  puts contact ? "#{contact.lastname}, #{contact.firstname} (#{contact.email})#{phone_mash}" : "#{ARGV[1]} (not found)"
+elsif ARGV[0] == "find_first" && ARGV[1]
   array_of_contacts = Contact.find_all_by_firstname((ARGV[1]))
-  array_of_contacts.each { |contact| puts "#{contact.lastname}, #{contact.firstname} (#{contact.email})" }
-elsif ARGV[0] == "findlast" && ARGV[1]
+  array_of_contacts.each do |contact| 
+    phone_mash = ""
+    contact.phone.each { |phone_hash| phone_mash << " | #{phone_hash[:phone]} (#{phone_hash[:label]})" }
+    puts "#{contact.id}: #{contact.lastname}, #{contact.firstname} (#{contact.email})#{phone_mash}"
+  end
+elsif ARGV[0] == "find_last" && ARGV[1]
   array_of_contacts = Contact.find_all_by_lastname((ARGV[1]))
-  array_of_contacts.each { |contact| puts "#{contact.lastname}, #{contact.firstname} (#{contact.email})" }
+  array_of_contacts.each do |contact| 
+    phone_mash = ""
+    contact.phone.each { |phone_hash| phone_mash << " | #{phone_hash[:phone]} (#{phone_hash[:label]})" }
+    puts "#{contact.id}: #{contact.lastname}, #{contact.firstname} (#{contact.email})#{phone_mash}"
+  end
+elsif ARGV[0] == "find_email" && ARGV[1]
+  contact = Contact.find_by_email((ARGV[1]))
+  phone_mash = ""
+  contact.phone.each { |phone_hash| phone_mash << " | #{phone_hash[:phone]} (#{phone_hash[:label]})" }
+  puts contact ? "#{contact.id}: #{contact.lastname}, #{contact.firstname} (#{contact.email})#{phone_mash}" : "#{ARGV[1]} (not found)"
+elsif ARGV[0] == "delete" && ARGV[1]
+  Contact.destroy(ARGV[1].to_i)
+  puts "ID-#{ARGV[1].to_i} was deleted..."
+elsif ARGV[0] == "add_phone" && ARGV[1] && ARGV[2] && ARGV[3]
+  mod_contact = Contact.find(ARGV[1])
+  mod_contact.add_phone([{ phone: ARGV[3], label: ARGV[2]}]) if mod_contact # not error checking for order of input
 end
